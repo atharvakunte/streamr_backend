40b30330a8d9bc3c33e168f04c21f3c13526ebf5
Atharva Kunte <36255091+atharvakunte@users.noreply.github.com>
2022-12-05 10:07:38 +0530

    added playlist controller

---
+++ controllers/playlist.js
@@ -0,0 +1,49 @@
+const playlist = require('../models/playlist');
+const Playlist = require('../models/playlist');
+
+
+//create Playlist
+exports.createPlaylist = (req, res) =>{
+    const playlist = new Playlist(req.body);
+    playlist.user = req.profile;
+    playlist.save((err, playlist)=>{
+        if(err){
+            return res.status(400).json({
+                error:"Please enter all required fields"
+            })
+        }
+        res.json({
+            playlistName:playlist.name,
+            createdBy:playlist.user.firstname
+        })
+    })
+}
+
+//get playlist by id
+exports.getPlaylistById = (req, res, next, id)=>{
+    Playlist.findById(id).exec((err, playlist)=>{
+        if(err){
+            return res.status(400).json({
+                error:"Playlist not found"
+            })
+        }
+        req.playlist = playlist;
+        next();
+    })
+}
+
+//get playlist by user
+exports.getPlaylistByUser = (req, res)=>{
+    Playlist.findOne().exec((err, playlist))
+}
+
+// get all Playlists
+
+
+// delete Playlists
+
+
+//add song to playlist
+
+
+//remove song from playlist
